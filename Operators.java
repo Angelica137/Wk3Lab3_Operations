26551df205b59f3bab09ab7d95c9f6dca3ec6f25
Angelica <Angelica137@users.noreply.github.com>
2021-03-26 21:46:19 +0000

    create Operators object

---
+++ Operators.java
@@ -0,0 +1,13 @@
+// Operator class takes user input from terminal
+// carries operation and displays result
+
+public class Operators {
+
+	// atributes
+	private String input;
+
+	// constructor
+	public Operators(String inputIn) {
+		input = inputIn;
+	}
+}
---
+++ OperatorsTester.java
@@ -0,0 +1,11 @@
+// test Operators class
+
+public class OperatorsTester {
+	public static void main(String[] args) {
+		// create Operators object
+		Operators o = new Operators("Hello, world!");
+		System.out.println(o);
+
+	}
+
+}
