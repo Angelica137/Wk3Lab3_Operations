5f342c8e1a79a2564c93fed903afd80fca47bf6d
Angelica <Angelica137@users.noreply.github.com>
2021-03-26 22:16:34 +0000

    create object from user input

--- Operators.java
+++ Operators.java
@@ -1,5 +1,7 @@
+
 // Operator class takes user input from terminal
 // carries operation and displays result
+import java.util.Scanner;
 
 public class Operators {
 
@@ -10,4 +12,16 @@ public class Operators {
 	public Operators(String inputIn) {
 		input = inputIn;
 	}
+
+	// got input from command line
+	public static void main(String[] args) {
+		Scanner user_input = new Scanner(System.in);
+		System.out.print("java Operators ");
+		String command = user_input.next();
+
+		// instnaitae an object from user input
+		Operators o1 = new Operators(command);
+		System.out.println(o1);
+	}
+
 }
